1b827a249c3087e9370ba90639b7827c187ca388
Airhard <Airhard@users.noreply.github.com>
2024-08-19 19:03:10 +0000

    ✅ Ready to clone and code.

--- bs/ext/commands.py
+++ bs/ext/commands.py
@@ -1,8 +1,8 @@
 import click
 
-from project_name.ext.auth import create_user
-from project_name.ext.database import db
-from project_name.models import Product
+from bs.ext.auth import create_user
+from bs.ext.database import db
+from bs.models import Product
 
 
 def create_db():
